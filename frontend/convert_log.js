896f6e5d99e0d5e68906a12d9023c18c779a2011
leluongbaonguyen <leluongbaonguyen@gmail.com>
2026-04-12 02:03:26 +0700

    Update: clean project files and push to GR50

--- frontend/convert_log.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-try {
-    const content = fs.readFileSync('start_log.txt', 'utf16le');
-    fs.writeFileSync('start_log_utf8.txt', content, 'utf8');
-} catch (e) {
-    console.error(e);
-}
